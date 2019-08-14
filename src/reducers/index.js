dc02fd030bce2c40af54056b1900591f051dad3c
Vicky <vyue013@gmail.com>
2019-08-14 13:51:15 -0700

    Set up initial reducer and state, fixed CSS height

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+const initialState = {
+    features: [
+        {
+            name: 'Matte black wrap',
+            price: 2000,
+            id: 6576237748
+        }
+    ]
+};
+
+export const carReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // case 'ADD_MEMBER':
+        //     return {
+        //         ...state,
+        //         members: [
+        //             ...state.members,
+        //             { name: action.payload, dragonStatus: false }
+        //         ]
+        //     };
+        default:
+            return state;
+    }
+};
